dfe5d0d8df58df604c5498bc7fc17fddb823ea05
Nicolas Rodriguez <nirodriguezp@unal.edu.co>
2021-02-26 15:37:01 +0000

    Mensaje

---
+++ hola.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+// int agrc char **argv para leer argumentos de la linea de comando
+int main(int argc, char **argv){
+  std::cout << "Hola mundo\n";
+  return 0; // 0 errores al sistema operativo
+}
