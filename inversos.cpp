237bf1d67bf789077356452beddf0354f8326223
nirodriguezp <nirodriguezp@unal.edu.co>
2021-03-05 15:27:51 +0000

    Last version

---
+++ inversos.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+void print(int min, int max);
+
+int main(void){
+  int m=10;
+  int n=1500;
+  print(m,n);
+}
+
+void print(int min, int max){
+  double sum;
+  double x;
+  for(double i=min; i<=max ; i++){
+    x=1/(i*i);
+    sum+=x;
+  }
+  std::cout<< sum << "\n";
+}
