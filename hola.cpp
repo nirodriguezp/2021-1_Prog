de775c1e625be28c1f3382e7011793d2bc7c4985
nirodriguezp <nirodriguezp@unal.edu.co>
2021-03-05 15:22:16 +0000

    Last version

--- hola.cpp
+++ hola.cpp
@@ -3,5 +3,7 @@
 // int agrc char **argv para leer argumentos de la linea de comando
 int main(int argc, char **argv){
   std::cout << "Hola mundo\n";
+  int m=1;
+  std::cout<<"m = "<<m<<"\n";
   return 0; // 0 errores al sistema operativo
 }
